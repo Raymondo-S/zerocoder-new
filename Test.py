8509bd9dfc99f6a3793cbc0d88f18f4d37a7a92e
Raymondo-S <phelixred@yahoo.com>
2025-06-05 17:50:43 +0300

    add new code

--- Test.py
+++ Test.py
@@ -1,2 +1,28 @@
-print('test')
-print('hi')
+import tkinter as tk
+
+def on_board(): # Условие для ввода данных от юзера
+    user_input = entry.get()
+    label.config(text=f' Hello, {user_input} ! ')
+
+def exit_program(): # Условие для закрытия программы
+    root.destroy() # Краш программы и закрытие окна
+
+root = tk.Tk() # Формируем окно
+root.title('Приветствие в окне') # Название окна
+
+label = tk.Label(root, text=" Введи свое имя ") # Текст в окне
+label.pack() # Сохранение текста в окне
+
+entry = tk.Entry(root) # Поле для ввода данных
+entry.pack() # Сохранение поля для ввода данных в окне
+
+button = tk.Button(root, text=f'Жми для приветствия!', command=on_board) # Кнопка для отработки запроса
+button.pack() # Сохранение кнопки в окне
+
+exit_button = tk.Button(root, text="Выход", command=exit_program) # Кнопка выхода из программы
+exit_button.pack() # Сохранение кнопки в окне
+
+
+
+
+root.mainloop()
